3b7b82fb7d194f0409746bab67b3254a89e155db
chetanade1 <chetan.ade@in.ncs-i.com>
2025-03-19 17:45:25 +0530

    '1'

---
+++ tests/HandleRadioButtons.spec.js
@@ -0,0 +1,57 @@
+//Handle Radio Buttons
+
+/**
+ Chetan Ade
+ */
+
+
+import {test, expect} from '@playwright/test';
+
+test('Handle Radio Buttons', async ({page}) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    console.log("Browser opened");
+
+    //Radio button visible or not
+    const maleRadioButton = await page.locator('#male');
+    await expect(maleRadioButton).toBeVisible();
+    console.log("Radio button is visible");
+
+    //Radio button Clicked
+    const maleRadioButtonChecked = await page.locator('#male');
+    await expect(maleRadioButtonChecked).toBeChecked;
+    console.log("Radio button is checked");
+
+    //radio button
+    //await page.locator("//input[@value='male']").check();
+    await page.check("//input[@value='male']");
+    console.log("Radio button clicked");
+
+    //Radio button checked or not
+    await expect(page.locator('#male').isChecked()).toBeTruthy();
+    console.log("Radio button is checked");
+
+    //Female radio button visible or not
+
+    const femaleRadioButton = await page.locator("//input[@value='female']");
+    await expect(femaleRadioButton).toBeVisible();
+    console.log("Radio button is visible");
+
+
+    await expect(page.locator('#female').isChecked()).toBeTruthy();
+    console.log("Radio button is checked");
+
+    await page.check('#female');
+    console.log("Radio button clicked");
+
+
+
+
+
+
+    await page.waitForTimeout(5000);
+
+    //close the browser
+    await page.close();
+
+})
---
+++ tests/inputBox.spec.js
@@ -0,0 +1,43 @@
+import {test , expect} from '@playwright/test';
+
+test('Handle inputBox', async ({page}) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    console.log("Browser opened");
+
+    //Textbox visible or not
+    const nameTextbox = await page.locator("//input[@id='name']");
+    await expect(nameTextbox).toBeVisible();
+    console.log("Textbox is visible");
+
+    //Textbox empty or not
+    const nameTextbox1 = await page.locator("//input[@id='name']");
+    await expect(nameTextbox1).toBeEmpty;
+    console.log("Textbox is empty");
+
+    //textbox editable or not
+    const nameTextbox2 = await page.locator("//input[@id='name']");
+    await expect(nameTextbox2).toBeEditable;
+    console.log("Textbox is editable");
+
+    //textbox enabled or not
+    const nameTextbox3 = await page.locator("//input[@id='name']");
+    await expect(nameTextbox3).toBeEnabled;
+    console.log("Textbox is enabled");
+
+
+    //Enter the name
+    //await page.getByRole('textbox', {name: 'name'}).fill('Pavan');
+    //await page.fill('id=name', 'pavan');
+    await page.locator('#name').fill('Chetan');
+    console.log("Name entered");
+
+    //Textbox empty or not
+    const nameTextbox4 = await page.locator("//input[@id='name']");
+    await expect(nameTextbox4).toBeEmpty;
+    console.log("Textbox is empty");
+    
+
+})
+
+
